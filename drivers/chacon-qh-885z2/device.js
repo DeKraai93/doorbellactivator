c0817c210485837c79d91f1abbbca860939fcfa9
DeKraai93 <61016055+DeKraai93@users.noreply.github.com>
2026-05-08 16:37:16 +0200

    added capability, alarm_generic

--- drivers/chacon-qh-885z2/device.js
+++ drivers/chacon-qh-885z2/device.js
@@ -6,6 +6,24 @@ class DeviceChaconNewTransmitter extends RFDevice {
   static RX_ENABLED = true;
 
   async onCommandFirst(command) {
+    // Zet status aan
+    await this.setCapabilityValue('alarm_generic', true);
+
+    // Reset bestaande timer
+    if (this._resetTimer) {
+      clearTimeout(this._resetTimer);
+    }
+
+    // Zet status na 2 seconden weer uit
+    this._resetTimer = setTimeout(async () => {
+      try {
+        await this.setCapabilityValue('alarm_generic', false);
+      } catch (err) {
+        this.error(err);
+      }
+    }, 2000);
+
+    // Trigger flow
     await this.homey.flow
       .getDeviceTriggerCard('QH-885Z2:received')
       .trigger(this, {}, command);
