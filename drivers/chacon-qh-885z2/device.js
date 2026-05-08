0638a0c6aea84f9d65bb86b53225fa88030fc840
DeKraai93 <61016055+DeKraai93@users.noreply.github.com>
2026-05-08 13:17:46 +0200

    stable version

--- drivers/chacon-qh-885z2/device.js
+++ drivers/chacon-qh-885z2/device.js
@@ -2,26 +2,18 @@
 
 const { RFDevice } = require('homey-rfdriver');
 
-// Combined class (all functionalities merged here)
 class DeviceChaconNewTransmitter extends RFDevice {
-
   static RX_ENABLED = true;
 
-  // Method from file 1: onCommandFirst
   async onCommandFirst(command) {
-    // Trigger the flow whenever a command is received (no need to check for specific payloads)
     await this.homey.flow
       .getDeviceTriggerCard('QH-885Z2:received')
       .trigger(this, {}, command);
   }
 
-  // Method from file 2: onCommandMatch
   async onCommandMatch(command) {
-    const { address } = await this.getData();
-    return address === command.address;
+    return command?.address === 'chacon-qh-885z2-doorbell';
   }
-
-  // You can add any custom logic from RFDevice class here if necessary.
 }
 
 module.exports = DeviceChaconNewTransmitter;
